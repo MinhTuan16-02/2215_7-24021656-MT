3e8d9e7a9dd47f0beb02312eafc9b8429a74a9d0
MinhTuan16-02 <24021656@vnu.edu.vn>
2025-03-17 10:13:23 +0700

    2215-7 24021656

--- AdvProg_L0-Hello/hello.cpp
+++ AdvProg_L0-Hello/hello.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
+
+std::string hello() {
+    return "Game Over!";
+}
+
+int main() {
+    std::cout << hello() << std::endl;
+    return 0;
+}
+#include <iostream>
 #include "hello.h"
 
 std::string printGameOver(){
 	// TODO: Return the required string
-	return "";
+	return "Hello World!";
 }
